8bff31badad80a5855a5cc8c8226210f49dba5eb
AlfanFG <alfanfaturahman10@gmail.com>
2021-08-05 21:48:09 +0700

    Fix Modal Pegawai + sweetalert2

--- application/views/parts/footer.php
+++ application/views/parts/footer.php
@@ -10,19 +10,21 @@
 </footer>
 </div>
 </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 <script src="<?= base_url('') ?>assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-<script src="<?= base_url('') ?>assets/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="<?= base_url('') ?>assets/js/bootstrap.bundle.min.js"></script> -->
 
-<script src="<?= base_url('') ?>assets/vendors/apexcharts/apexcharts.js"></script>
-<script src="<?= base_url('') ?>assets/js/pages/dashboard.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.js"></script>
+
+
+<!-- <script src="<?= base_url('') ?>assets/js/pages/dashboard.js"></script> -->
 
 <script src="<?= base_url('') ?>assets/js/main.js"></script>
 
-
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-sweetalert/1.0.1/sweetalert.min.js"></script>
--- application/views/parts/header.php
+++ application/views/parts/header.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.css') ?>">
-
+    <link href="//cdn.jsdelivr.net/npm/@sweetalert2/theme-default@4/default.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url('') ?>assets/vendors/iconly/bold.css">
 
     <link rel="stylesheet" href="<?= base_url('') ?>assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
@@ -18,5 +18,7 @@
     <link rel="shortcut icon" href="<?= base_url('') ?>assets/images/favicon.svg" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
-    
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
+
+
 </head>
